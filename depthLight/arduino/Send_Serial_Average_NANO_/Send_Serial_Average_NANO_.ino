623e4bab4a575bdf27a212c9310db1996eaafdc6
McKennon McMillian <mbmcmill@go.olemiss.edu>
2017-06-15 20:56:11 -0500

    Update Send_Serial_Average_NANO_.ino

--- depthLight/arduino/Send_Serial_Average_NANO_/Send_Serial_Average_NANO_.ino
+++ depthLight/arduino/Send_Serial_Average_NANO_/Send_Serial_Average_NANO_.ino
@@ -4,18 +4,18 @@ http://tolsonwinters.com/a/HC_SR04_Range_Finder.ino
 HC-SR04 Ping distance sensor
 VCC to Arduino 5V
 GND to Arduino GND
-Echo to Arduino pin 13
-Trig to Arduino pin 12
-More info at: http://goo.gl/kJ8Gl
+Echo to Arduino pin 2
+Trig to Arduino pin 3
 Original code improvements to the Ping sketch sourced from Trollmaker.com
 Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
 Modified by Tolson Winters (Aug 27, 2014) for simplified serial monitor reading.
-Modified by McKennon McMillian (Feb 12, 2017) inch convertion and simplified distance calculation.
+Modified by McKennon McMillian
 */
 
+//Define pins and array size
 #define trigPin 3
 #define echoPin 2
-#define SIZE 20
+#define SIZE 20 //Size of array
 
 
 void setup() {
@@ -35,16 +35,17 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = duration / 58.2;
+  distance = duration / 58.2; // time/speedOfSound 
   
     sum += distance;
   }
   average = float(sum) / SIZE;
+  //The HC-SR04 has a max read distance 4 meters. If the average is greater than 400cm average is set to 400
   if(average > 400){
     average = 400;
   }
     Serial.flush();
-    Serial.print(average);
+    Serial.print(average); //Send average as cm
     Serial.println();
   
   delay(10);
