6f9c51bcf34f4a4b2a740ff3ff64a4c6c58346c2
McKennon McMillian <mbmcmill@go.olemiss.edu>
2017-06-15 22:31:13 -0500

    Update scale.cs

--- depthLight/C#/scale.cs
+++ depthLight/C#/scale.cs
@@ -12,7 +12,7 @@ public class scale : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float scale = parent.GetComponent<Combo_Cam_Serial>().scale/2f;
+        float scale = parent.GetComponent<Combo_Cam_Serial>().scale/2f; //Scales with the parent object
         transform.localScale = new Vector3(scale,scale, scale);
     }
 }
