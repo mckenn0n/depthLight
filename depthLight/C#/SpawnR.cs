dcdb4c9468a67944d8607c31704c3875eec27c42
McKennon McMillian <mbmcmill@go.olemiss.edu>
2019-10-08 16:08:13 -0500

    Update for new camera

--- depthLight/C#/SpawnR.cs
+++ depthLight/C#/SpawnR.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class SpawnR : MonoBehaviour
 {
-
     public GameObject cam, bottom, top, topR, topL, botL, botR; //attach all line end points in unity3D editer
     public bool on = true; //this is a check box in the unity3D editer that can be turned on and off
     public GameObject particles; //attach particals if wanted in unity3D editer
+    public GameObject location;
     bool triggerButtonDown = false;
     Color custColor;
 
@@ -17,12 +17,12 @@ public class SpawnR : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        particles.GetComponent<Renderer>().enabled = 
-            bottom.GetComponent<Renderer>().enabled = 
-            top.GetComponent<Renderer>().enabled = 
-            topL.GetComponent<Renderer>().enabled = 
-            topR.GetComponent<Renderer>().enabled = 
-            botR.GetComponent<Renderer>().enabled = 
+        particles.GetComponent<Renderer>().enabled =
+            bottom.GetComponent<Renderer>().enabled =
+            top.GetComponent<Renderer>().enabled =
+            topL.GetComponent<Renderer>().enabled =
+            topR.GetComponent<Renderer>().enabled =
+            botR.GetComponent<Renderer>().enabled =
             botL.GetComponent<Renderer>().enabled =
         cam.GetComponent<Renderer>().enabled = false;
 
@@ -34,28 +34,27 @@ public class SpawnR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (on)
         {
             triggerButtonDown = controller.GetHairTrigger();
 
             if (triggerButtonDown)
             {
-               /*
-               When on is true and the trigger is pulled down the webCam object is rendered and all the lines are drawn
-               */
+                /*
+                When on is true and the trigger is pulled down the webCam object is rendered and all the lines are drawn
+                */
                 cam.GetComponent<Renderer>().enabled = true;
                 particles.GetComponent<Renderer>().enabled = true;
-                DrawLine(trackedObj.transform.position, bottom.transform.position, custColor);
-                DrawLine(trackedObj.transform.position, botR.transform.position, custColor);
-                DrawLine(trackedObj.transform.position, botL.transform.position, custColor);
-                DrawLine(trackedObj.transform.position, top.transform.position, custColor);
-                DrawLine(trackedObj.transform.position, topL.transform.position, custColor);
-                DrawLine(trackedObj.transform.position, topR.transform.position, custColor);
+                DrawLine(location.transform.position, bottom.transform.position, custColor);
+                DrawLine(location.transform.position, botR.transform.position, custColor);
+                DrawLine(location.transform.position, botL.transform.position, custColor);
+                DrawLine(location.transform.position, top.transform.position, custColor);
+                DrawLine(location.transform.position, topL.transform.position, custColor);
+                DrawLine(location.transform.position, topR.transform.position, custColor);
             }
             else if (!triggerButtonDown)
             {
-            //objects no longer rendered
+                //objects no longer rendered
                 cam.GetComponent<Renderer>().enabled = false;
                 particles.GetComponent<Renderer>().enabled = false;
             }
@@ -63,20 +62,22 @@ public class SpawnR : MonoBehaviour
         else
         {
             cam.GetComponent<Renderer>().enabled = false;
-            particles.GetComponent<Renderer>().enabled=false;
+            particles.GetComponent<Renderer>().enabled = false;
         }
 
     }
-    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.05f){
-    /*
-    This function draws lines from the controller to the to the end points placed on an object
-    This is to keep the user from getting VR sick by showing them it is a projection
-    */
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.05f)
+    {
+        /*
+        This function draws lines from the controller to the to the end points placed on an object
+        This is to keep the user from getting VR sick by showing them it is a projection
+        */
         GameObject myLine = new GameObject();
+        myLine.layer = 8;  //Always on top
         myLine.transform.position = start;
         myLine.AddComponent<LineRenderer>();
         LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        //lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
         lr.startColor = color;
         lr.endColor = color;
         lr.startWidth = 0.001f;
@@ -86,4 +87,3 @@ public class SpawnR : MonoBehaviour
         GameObject.Destroy(myLine, duration);
     }
 }
-
